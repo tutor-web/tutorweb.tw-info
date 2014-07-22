28412dc63e23ca603249bb61b4e85417d574c078
Jamie Lentin <jm@lentin.co.uk>
2014-07-22 15:10:40 +0100

    Empty tw-info package

---
+++ README.rst
@@ -0,0 +1,6 @@
+Tutorweb tutor-web.info site
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Policy for the tutor-web.info site.
+
+See https://github.com/tutorweb/tutorweb.buildout for more information. 
---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name='tutorweb.tw-info',
+    version='0.1.dev0',
+    description='Tutorweb content tw-info',
+    long_description=read('README.rst') +
+                     read('HISTORY.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='plone tutorweb',
+    author='Jamie Lentin',
+    author_email='lentinj@shuttlethread.com',
+    url='https://github.com/tutorweb/tutorweb.tw-info',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['tutorweb'],
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'tutorweb.content',
+        ],
+    },
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+    include_package_data=True,
+    zip_safe=False,
+)
